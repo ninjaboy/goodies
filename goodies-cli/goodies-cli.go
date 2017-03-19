6fda8ac01883171240c17121da7b75615f7f56b5
ninjaboy <extraparanoid@gmail.com>
2017-03-19 22:15:27 +0000

    Some minor refactoring. Will return to this later

--- goodies-cli/goodies-cli.go
+++ goodies-cli/goodies-cli.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"goodies/goodies-server"
 )
 
 func main() {
-	fmt.Println("Connecting to 127.0.0.1:6009")
-	res, err := http.Get("http://127.0.0.1:6009/ping")
+	fmt.Println("Connecting to 127.0.0.1:9006")
+	
+	json.Marshal()
+	res, err := http.Post("http://127.0.0.1:9006/goodies",
+		"application/json",
+		bytes.NewReader())
 	if err != nil {
 		log.Fatal(err)
 	}
--- goodies-server/main.go
+++ goodies-server/main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"goodies/goodies"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 	"time"
-
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/fasthttp"
 )
+
+type Command struct {
+	Name   string
+	Params []string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var cmd Command
+	err := decoder.Decode(&cmd)
+	if err != nil {
+		fmt.Fprintf(w, formatError("REQUEST-PARSING-FAILED cannot parse request body"))
+		return
+	}
+	fmt.Fprintf(w, handleCommand(cmd))
+}
+
+func handleCommand(cmd Command) string {
+	return cmd.Params[0]
+}
+
+func formatError(err string) string {
+	return fmt.Sprintf("error %v", err)
+}
 
 func main() {
 	g := goodies.NewGoodies(1*time.Minute, "./goodies.dat", 30*time.Second)
-	e := createEcho(g)
-	go e.Run(fasthttp.New(":6009"))
+	http.HandleFunc("/goodies", handler)
+	http.ListenAndServe(":9006", nil) //9006 as for good
 
 	fmt.Println("Enter any text to exit")
 	reader := bufio.NewReader(os.Stdin)
@@ -32,51 +53,51 @@ func main() {
 	fmt.Println("Bye")
 }
 
-func createEcho(g *goodies.Goodies) *echo.Echo {
-	e := echo.New()
-	e.SetDebug(true)
+// func createEcho(g *goodies.Goodies) *echo.Echo {
+// 	e := echo.New()
+// 	e.SetDebug(true)
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "PONG")
-	})
+// 	e.GET("/ping", func(c echo.Context) error {
+// 		return c.String(http.StatusOK, "PONG")
+// 	})
 
-	e.GET("/values/:key", func(c echo.Context) error {
-		value, found := g.Get(c.Param("key"))
-		if found {
-			return c.String(http.StatusOK, fmt.Sprintf("%#v", value))
-		}
-		return c.String(http.StatusNotFound, "Requested key not found")
-	})
+// 	e.GET("/values/:key", func(c echo.Context) error {
+// 		value, found := g.Get(c.Param("key"))
+// 		if found {
+// 			return c.String(http.StatusOK, fmt.Sprintf("%#v", value))
+// 		}
+// 		return c.String(http.StatusNotFound, "Requested key not found")
+// 	})
 
-	e.POST("/values/", func(c echo.Context) error {
-		key := c.FormValue("key")
-		value := c.FormValue("value")
-		ttlSec := c.FormValue("ttl_sec")
-		var ttl time.Duration
-		if ttlSec != "" {
-			seconds, err := strconv.Atoi(ttlSec)
-			if err == nil {
-				ttl = time.Duration(seconds) * time.Second
-			} else {
-				return err
-			}
-		} else {
-			ttl = goodies.ExpireDefault
-		}
+// 	e.POST("/values/", func(c echo.Context) error {
+// 		key := c.FormValue("key")
+// 		value := c.FormValue("value")
+// 		ttlSec := c.FormValue("ttl_sec")
+// 		var ttl time.Duration
+// 		if ttlSec != "" {
+// 			seconds, err := strconv.Atoi(ttlSec)
+// 			if err == nil {
+// 				ttl = time.Duration(seconds) * time.Second
+// 			} else {
+// 				return err
+// 			}
+// 		} else {
+// 			ttl = goodies.ExpireDefault
+// 		}
 
-		g.Set(key, value, ttl)
-		return c.String(http.StatusCreated, "Key created successfully")
-	})
+// 		g.Set(key, value, ttl)
+// 		return c.String(http.StatusCreated, "Key created successfully")
+// 	})
 
-	e.DELETE("/values/:key", func(c echo.Context) error {
-		key := c.Param("key")
-		g.Remove(key)
-		return c.String(http.StatusOK, "Success")
-	})
+// 	e.DELETE("/values/:key", func(c echo.Context) error {
+// 		key := c.Param("key")
+// 		g.Remove(key)
+// 		return c.String(http.StatusOK, "Success")
+// 	})
 
-	e.GET("/keys/", func(c echo.Context) error {
-		res := "[" + strings.Join(g.Keys(), ";") + "]"
-		return c.String(http.StatusOK, res)
-	})
-	return e
-}
+// 	e.GET("/keys/", func(c echo.Context) error {
+// 		res := "[" + strings.Join(g.Keys(), ";") + "]"
+// 		return c.String(http.StatusOK, res)
+// 	})
+// 	return e
+// }
